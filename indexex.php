91abb5852e4677884c22ed625f79581d936b7bc0
Kuznetsova15 <113414850+Kuznetsova15@users.noreply.github.com>
2023-04-04 14:15:53 +0300

    Update indexex.php

--- indexex.php
+++ indexex.php
@@ -15,3 +15,74 @@ $pol = $_POST['radio-1'];
 $limbs = intval($_POST['radio-2']);
 $superpowers = array($_POST['super']);
 $bio= $_POST['bio'];
+
+$bioreg = "/^\s*\w+[\w\s\.,-]*$/";
+$reg = "/^\w+[\w\s-]*$/";
+$mailreg = "/^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/";
+$list_sup = array('inv','walk','fly');
+
+if(!preg_match($reg,$name)){
+	print_r('Неверный формат имени');
+	exit();
+}
+if($limbs == 0){
+	print_r('Нет конечностей');
+	exit();
+}
+if(!preg_match($bioreg,$bio)){
+	print_r('Неверный формат биографии');
+	exit();
+}
+if(!preg_match($mailreg,$email)){
+	print_r('Неверный формат email');
+	exit();
+}
+if($pol !== 'male' && $pol !== 'female'){
+	print_r('Неверный формат пола');
+	exit();
+}
+foreach($superpowers as $checking){
+	if(array_search($checking,$list_sup)=== false){
+			print_r('Неверный формат суперсил');
+			exit();
+	}
+}
+
+$user = 'u52978';
+$pass = '4644833';
+$db = new PDO('mysql:host=localhost;dbname=u52978', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+try {
+  $stmt = $db->prepare("INSERT INTO form SET name=:name, email=:email, year=:byear, pol=:pol, limbs=:limbs, bio=:bio");
+  $stmt->bindParam(':name', $name);
+  $stmt->bindParam(':email', $email);
+  $stmt->bindParam(':byear', $birth_year);
+  $stmt->bindParam(':pol', $pol);
+  $stmt->bindParam(':limbs', $limbs);
+  $stmt->bindParam(':bio', $bio);
+
+  if($stmt->execute()==false){
+  print_r($stmt->errorCode());
+  print_r($stmt->errorInfo());
+  exit();
+  }
+  
+  
+  $id = $db->lastInsertId();
+  $sppe= $db->prepare("INSERT INTO super SET name=:name, per_id=:person");
+  $sppe->bindParam(':person', $id);
+  foreach($superpowers as $inserting){
+	$sppe->bindParam(':name', $inserting);
+	if($sppe->execute()==false){
+	  print_r($sppe->errorCode());
+	  print_r($sppe->errorInfo());
+	  exit();
+	}
+  }
+}
+catch(PDOException $e){
+  print('Error : ' . $e->getMessage());
+  exit();
+}
+
+print_r("Данные отправлены в бд");
+?>
